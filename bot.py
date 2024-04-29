418b65751f7ebe7323911803cc0c2bef5afc1f96
Lucian <73092546+Flyingbacen@users.noreply.github.com>
2024-04-29 01:38:07 +0000

    base added

---
+++ bot.py
@@ -0,0 +1,21 @@
+# necessary imports
+""""""
+import discord
+import asyncio
+""""""
+import json
+""""""
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+""""""
+@tree.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@client.event
+async def on_ready():
+	await tree.sync()
+	print("Ready!")
+
+client.run(json.load(open("config.json"))["token"])
